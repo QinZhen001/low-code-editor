24427ff9d28f8ef38c7655f10720235a22563e39
qinzhen <qz519189636@qq.com>
2022-07-13 21:08:04 +0800

    feat: toolbar insert

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -38,8 +38,7 @@
 <script>
 import { generateID, toast } from "../utils/index.js";
 import eventBus from "../utils/eventBus";
-import { mapState } from 'vuex'
-
+import { mapState } from "vuex";
 
 export default {
   components: {
@@ -53,7 +52,96 @@ export default {
       timer: null,
     };
   },
+  computed: mapState([
+    "componentData",
+    "canvasStyleData",
+    "areaData",
+    "curComponent",
+    "curComponentIndex",
+  ]),
+  created() {
+    eventBus.$on("preview", this.preview);
+    eventBus.$on("save", this.save);
+    eventBus.$on("clearCanvas", this.clearCanvas);
+    this.scale = this.canvasStyleData.scale;
+  },
+  methods: {
+    format(value) {},
+    getOriginStyle(value) {},
+    handleScaleChange() {},
+    lock() {},
+    unlock() {},
+    compose() {},
+    decompose() {},
+    undo() {},
+    redo() {},
+    handleFileChange() {},
+    preview() {},
+    save() {},
+    clearCanvas() {},
+    handlePreviewChange() {},
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.toolbar {
+  padding: 15px 10px;
+  white-space: nowrap;
+  overflow-x: auto;
+  background: #fff;
+  border-bottom: 1px solid #ddd;
+
+  .canvas-config {
+    display: inline-block;
+    margin-left: 10px;
+    font-size: 14px;
+    color: #606266;
+
+    input {
+      width: 50px;
+      margin-left: 10px;
+      outline: none;
+      padding: 0 5px;
+      border: 1px solid #ddd;
+      color: #606266;
+    }
+
+    span {
+      margin-left: 10px;
+    }
+  }
+
+  .insert {
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background: #fff;
+    border: 1px solid #dcdfe6;
+    color: #606266;
+    -webkit-appearance: none;
+    text-align: center;
+    box-sizing: border-box;
+    outline: 0;
+    margin: 0;
+    transition: 0.1s;
+    font-weight: 500;
+    padding: 9px 15px;
+    font-size: 12px;
+    border-radius: 3px;
+    margin-left: 10px;
+
+    &:active {
+      color: #3a8ee6;
+      border-color: #3a8ee6;
+      outline: 0;
+    }
+
+    &:hover {
+      background-color: #ecf5ff;
+      color: #3a8ee6;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
+import ElementUI from 'element-ui'
 import App from "./App.vue";
+import store from './store'
 import router from "./router/index.js";
 
 import "@/assets/iconfont/iconfont.css";
@@ -7,8 +9,13 @@ import "@/styles/animate.scss";
 import "element-ui/lib/theme-chalk/index.css";
 import "@/styles/reset.css";
 
+Vue.use(ElementUI, { size: 'small' })
+Vue.config.productionTip = false
+
+
 new Vue({
   el: "#app",
   router,
+  store,
   render: (h) => h(App),
 });
