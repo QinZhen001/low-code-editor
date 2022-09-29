91ca09ef110f7918f9ecb2fc1eb6910d26631e42
qinzhen <qz519189636@qq.com>
2022-09-29 17:45:14 +0800

    Update index.js

--- src/custom-component/index.js
+++ src/custom-component/index.js
@@ -5,9 +5,10 @@ export * from './component-list';
 const globalComponents = ['Picture', 'VText', 'VButton', 'Group', 'RectShape'];
 
 export function regiesterComponents() {
-  globalComponents.forEach((key) => {
-    import(/* @vite-ignore */ `./${key}.vue`).then((module) => {
-      Vue.component(key, module.default);
-    });
+  const globalComponents = import.meta.glob('./*.vue');
+  Object.keys(globalComponents).forEach((key) => {
+    console.log(key, globalComponents[key]);
+    const name = key.replace(/\.\/(.*)\.vue/, '$1');
+    Vue.component(name, globalComponents[key]);
   });
 }
